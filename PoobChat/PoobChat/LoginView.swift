ec4fda20d736054128eecd79e0924889b31954a4
Jnorth416 <josh.north416@gmail.com>
2022-12-26 15:24:19 -0800

    Created Login Screen

--- PoobChat/PoobChat/LoginView.swift
+++ PoobChat/PoobChat/LoginView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    
+    
     var body: some View {
-        Text("Login View")
+        VStack(spacing: 16){
+            Image(systemName: "ellipsis.message")
+                .font(.system(size: 150))
+                .scaledToFit()
+                .padding()
+            TextField("Username", text: $username)
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(40)
+                
+            TextField("Password", text: $password)
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(40)
+            
+            Button("Login"){
+                    
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.blue)
+                .cornerRadius(40)
+            
+                
+            Button("Create an Account Here"){
+                    
+            }
+        }
     }
 }
 
